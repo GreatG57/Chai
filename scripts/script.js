55305d44f7521624938db61974b172783e8d33d6
GreatG57 <145491498+GreatG57@users.noreply.github.com>
2024-09-04 14:07:05 +0000

    commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.getElementById('show-more-btn').addEventListener('click', function() {
+    var extraTeam = document.getElementById('extra-team');
+    if (extraTeam.style.display === 'none') {
+      extraTeam.style.display = 'block';
+      this.textContent = 'Show Less';
+    } else {
+      extraTeam.style.display = 'none';
+      this.textContent = 'Show More';
+    }
+  });
