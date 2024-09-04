d80b9ca47523eb75598b05e309a9ee8d8ad37f3a
GreatG57 <145491498+GreatG57@users.noreply.github.com>
2024-09-04 18:36:35 +0000

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,12 @@
+console.log("Script loaded");
+
 document.getElementById('show-more-btn').addEventListener('click', function() {
-    var extraTeam = document.getElementById('extra-team');
-  
-    if (extraTeam.style.maxHeight === '0px' || extraTeam.style.maxHeight === '') {
-      // Expand the section with a smooth transition
-      extraTeam.style.opacity = '1';
-      extraTeam.style.maxHeight = extraTeam.scrollHeight + 'px'; // Dynamically set the height
-      this.textContent = 'Show Less';
-    } else {
-      // Collapse the section with a smooth transition
-      extraTeam.style.opacity = '0';
-      extraTeam.style.maxHeight = '0px';
-      this.textContent = 'Show More';
-    }
-  });
-  
+  var extraTeam = document.getElementById('extra-team');
+  if (extraTeam.style.display === 'none' || extraTeam.style.display === '') {
+    extraTeam.style.display = 'block';
+    this.textContent = 'Show Less';
+  } else {
+    extraTeam.style.display = 'none';
+    this.textContent = 'Show More';
+  }
+});
